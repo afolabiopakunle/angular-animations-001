c6c294846bddb1ce901a2575028c0f770bef23d9
afolabiopakunle <opakunleafolabi@gmail.com>
2022-07-28 05:49:44 +0100

    fade in implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations'
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  animations: [
+    trigger('fadeIn', [
+      // state(),
+      transition('void => *', [
+        style({backgroundColor: 'green', opacity: 0}),
+        animate(500, style({backgroundColor: 'blue', opacity: 1}))
+      ]),
+      transition('* => void', [
+        style({opacity: 1}),
+        animate(500, style({opacity: 0}))
+      ])
+    ])
+  ]
 })
 export class AppComponent  {
   name = 'Angular';
+  show = true;
 }
